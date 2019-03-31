930bee73adff11bec739bf7ac6d056b15ada90dd
agustinBassi <jagustinbassi@gmail.com>
2019-03-30 22:21:01 -0300

    Crea controlador de filtros, lo testea y funciona todo. Primer etapa terminada.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from wav_file import test_wav_file_class
 from flanger_filter import test_flanger_filter_class
 from filters_view import test_filters_view_class
 from comb_filter import test_comb_filter_class
+from filters_controller import test_filters_controller_class
 
 def main():
     """Main method to run DSP Project.
@@ -23,6 +24,7 @@ def main():
     #test_wav_file_class()
     #test_flanger_filter_class()
     #test_filters_view_class()
-    test_comb_filter_class()
+    #test_comb_filter_class()
+    test_filters_controller_class()
 
 main()
